599573fc54b64e921d734c364545c803ec20f4ce
yclong <yongcheng.long@wustl.edu>
2019-10-20 21:23:26 -0500

    deal with url

--- Squad/urls.py
+++ Squad/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('explore/', include('explore.urls')),
-    path('myclasses/', include('myclasses.urls')),
-    path('accountprofile/', include('accountprofile.urls')),
+    path('dashboard/', include('myclasses.urls')),
+    path('account/', include('accountprofile.urls')),
     path('groups/', include('groups.urls')),
     path('class/', include('class.urls')),
     path('notification/', include('notification.urls'))
