7a63176f9201763c4f442b8beba30a02792ae2bc
yclong <yongcheng.long@wustl.edu>
2019-11-02 21:03:41 -0500

    url priority fixed

--- accountprofile/urls.py
+++ accountprofile/urls.py
@@ -4,6 +4,6 @@ from accountprofile import views
 urlpatterns = [
     path('', views.listRequestedmine),
     path('edit', views.changProfile, name='profedit'),
-    re_path(r'[a-zA-Z0-9]+', views.changebyclass),
+    re_path(r'[a-zA-Z][a-zA-Z]+', views.changebyclass),
     re_path(r'[0-9]+', views.listRequested),
 ]
