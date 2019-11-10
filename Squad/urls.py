6ee74b4a73cbc261d5cec1ab83457b2ab8115835
devilzhoukan <devilzhoukan@qq.com>
2019-11-09 23:28:16 -0600

    add jump to login

--- Squad/urls.py
+++ Squad/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import jump_to_login
 
 urlpatterns = [
+    path('', jump_to_login),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from accounts.forms import CustomRegistrationForm
-
+from django.http import HttpResponseRedirect
 
 class SignUp(generic.CreateView):
     form_class = CustomRegistrationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+def jump_to_login(request):
+    return HttpResponseRedirect('/accounts/login/')
