c7cafe404c1398e4bac4035db95872122c029c37
devilzhoukan <devilzhoukan@qq.com>
2019-11-09 23:14:11 -0600

    update to show group post notification

--- notification/urls.py
+++ notification/urls.py
@@ -3,6 +3,7 @@ from notification import views
 
 urlpatterns = [
     path('', views.notification_requested, name='notification'),
+    path('readall', views.read_all_notifications),
     re_path(r'read/[0-9]+', views.read_notification, name='globalread'),
     re_path(r'accept/[0-9]+', views.accept_notification),
     re_path(r'decline/[0-9]+', views.decline_notification),
