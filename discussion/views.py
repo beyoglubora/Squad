2107b67e4ec66d3a8d7cf803c7e6988ae4fb9837
yclong <yongcheng.long@wustl.edu>
2019-11-16 21:18:40 -0600

    discussion blueprint

--- discussion/views.py
+++ discussion/views.py
@@ -1,4 +1,25 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+import data as DataModel
 
 def show_messages(request):
     return render(request, 'discussion.html')
+
+
+def create_post(request):
+    # class post -> -5
+    # group post -> group id
+
+    return JsonResponse({"result": True})
+
+
+def delete_post(request):
+    # creator and instructor
+    # create parent -> all gone
+
+    return JsonResponse({"result": True})
+
+
+def edit_post(request):
+    # only creator can edit
+    return JsonResponse({"result": True})
