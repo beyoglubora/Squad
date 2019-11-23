b769bce7f6aa1241f8d42ee1e3590b417621cae9
beyoglubora <beyoglubora@wustl.edu>
2019-11-23 13:22:35 -0600

    added email activation functionality

--- Squad/settings.py
+++ Squad/settings.py
@@ -135,3 +135,8 @@ LOGOUT_REDIRECT_URL = "login"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'signupforsquad@gmail.com'
+EMAIL_HOST_PASSWORD = 'joinsquad@123'
+EMAIL_PORT = 587
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('activate/', views.account_activation, name='account_activation'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
