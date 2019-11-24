a5c9212e7c92905e88c3e82fede8b229588669b9
yclong <yongcheng.long@wustl.edu>
2019-11-23 23:59:41 -0600

    test 4 kan

--- assignments/forms.py
+++ assignments/forms.py
@@ -2,6 +2,14 @@ from django import forms
 from data.models import Assignment
 
 class AssignmentsForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['due_date'].widget.attrs.update({
+            'class': 'form-control datetimepicker-input',
+            'data-target': '#datetimepicker1'
+        })
+
     class Meta:
         model = Assignment
         fields = ('subject', 'description', 'due_date', 'class_instance')
