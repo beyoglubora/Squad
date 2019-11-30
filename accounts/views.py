de749b9d2a71d1f2b09510d700b1174f57ed89c9
beyoglubora <beyoglubora@wustl.edu>
2019-11-30 12:27:02 -0600

    deployment stuff

--- accounts/views.py
+++ accounts/views.py
@@ -26,7 +26,7 @@ def signup(request):
             message = render_to_string('acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
                 'token': account_activation_token.make_token(user),
             })
             to_email = form.cleaned_data.get('email')
